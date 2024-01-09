a34bbab93277be248bf9b2c7ea8ecb9fe5cc2083
therealrinku <rinkunited2032@gmail.com>
2024-01-09 13:08:27 +0545

    feat: scroll to bottom of chat on open

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -32,26 +32,41 @@ export default function Chatbox({ chatbotConfig }: any) {
       //@ts-ignore
       container.scrollTop = container.scrollHeight - container.clientHeight;
     }
-  }, [conversations.length]);
+  }, [conversations.length, showChat]);
 
   return (
     <Fragment>
       {showChat && (
-        <div className="fixed right-10 bottom-32 w-full max-w-[350px]">
+        <div className="fixed right-10 bottom-24 w-full max-w-[350px]">
           <section className="shadow-md mt-5 text-white border rounded-t-md rounded-b-md">
             <div
               style={{
                 backgroundColor: chatbotConfig.configurations?.titleBgColor,
                 color: chatbotConfig.configurations?.titleTextColor,
               }}
-              className="px-2 py-3 chatbot-title-bg rounded-t-md flex items-center gap-2"
+              className="px-2 py-3 chatbot-title-bg rounded-t-md flex items-center gap-2 justify-between"
             >
-              <img src={chatbotConfig.configurations?.icon} className="chatbot-icon object-cover w-5 h-5" />
-              <p>{chatbotConfig.configurations?.title}</p>
+              <section className="flex items-center gap-2">
+                <img
+                  src={chatbotConfig.configurations?.icon}
+                  className="chatbot-icon object-cover w-6 h-6 rounded-full"
+                />
+                <p>{chatbotConfig.configurations?.title}</p>
+              </section>
+
+              <button
+                style={{
+                  color: chatbotConfig.configurations?.titleTextColor,
+                }}
+                onClick={() => setShowChat((prev) => !prev)}
+              >
+                <IoClose size={24} />
+              </button>
             </div>
 
             <div className="bg-white px-2 h-[400px] p-3 overflow-y-auto" ref={messageViewRef}>
               {conversations.map((conversation, i) => {
+                // even indexed message is from chatbot
                 return (
                   <div
                     key={i}
@@ -77,13 +92,21 @@ export default function Chatbox({ chatbotConfig }: any) {
 
               <div className="w-[75%] float-right border rounded-t-md rounded-b-md mt-3">
                 {/*@ts-ignore */}
-                <p className="px-3 py-2 text-black text-sm">{replyOptions?.message}</p>
+                <p
+                  style={{
+                    backgroundColor: chatbotConfig.configurations?.botMessageBgColor,
+                    color: chatbotConfig.configurations?.botMessageTextColor,
+                  }}
+                  className="px-3 py-2 text-sm"
+                >
+                  {replyOptions?.message}
+                </p>
                 {/*@ts-ignore */}
                 {replyOptions?.replyOptions?.map((message: any, i: number) => {
                   return (
                     <button
                       onClick={(e) => nextMessage(e, message)}
-                      className={`px-3 text-left w-full text-black  text-sm py-2 border-t-2`}
+                      className={`hover:italic px-3 text-left w-full text-black  text-sm py-2 border-t-2`}
                       key={message.id}
                     >
                       {message.message}
@@ -96,13 +119,15 @@ export default function Chatbox({ chatbotConfig }: any) {
         </div>
       )}
 
-      <button
-        style={{ background: chatbotConfig.configurations?.btnBgColor }}
-        onClick={() => setShowChat((prev) => !prev)}
-        className="fixed bottom-10 right-10 mt-5 bg-green-500 h-12 w-12 rounded-full flex flex-col items-center justify-center"
-      >
-        {showChat ? <IoClose color="white" size={24} /> : <IoChatbubbleEllipsesOutline color="white" size={24} />}
-      </button>
+      {!showChat && (
+        <button
+          style={{ background: chatbotConfig.configurations?.btnBgColor }}
+          onClick={() => setShowChat((prev) => !prev)}
+          className="fixed bottom-10 right-10 mt-5 bg-green-500 h-12 w-12 rounded-full flex flex-col items-center justify-center"
+        >
+          <IoChatbubbleEllipsesOutline color="white" size={24} />
+        </button>
+      )}
     </Fragment>
   );
 }
